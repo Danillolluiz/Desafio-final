5ea090cd2e850ae3b01dfd16855ce60c07a09ed7
Danillolluiz <danillolluiz20@gmail.com>
2025-04-04 21:49:58 -0300

    database

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,7 +1,21 @@
 import S from "./doados.module.scss";
 import protagonista from "../../assets/protagonista.png";
+import axios from "axios";
+import { useState,useEffect } from "react";
 
 export default function Doados() {
+
+  const[livro, setlivros] = useState([])
+
+  const getlivros = async () => {
+    const response = await axios.get("https://projeto-api-1-rdkv.onrender.com/livros")
+  setlivros(response.data)  
+  }
+
+  useEffect(()=>{
+    getlivros()
+  },[])
+
   return (
     <section className={S.boxDoados}>
       <h2>Livros Doados</h2>
@@ -12,6 +26,14 @@ export default function Doados() {
           <p>Susanne Andrade</p>
           <p>Ficção</p>
         </article>
+        {livro.map((item)=>(
+          <article key={item.id}>
+            <img src={item.imagem_url} alt="" />
+            <h3>{item.titulo}</h3>
+            <p>{item.categoria}</p>
+            <p>{item.autor}</p>
+          </article>
+        ))}
       </section>
     </section>
   );
